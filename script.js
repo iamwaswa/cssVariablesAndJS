420bcda68df11d525802ed3ef4a39dfc62f592bf
waswaolunga <olungaw@gmail.com>
2018-10-15 17:41:24 -0700

    Rearrange javascript

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let inputs = document.querySelectorAll(`input`);
-
 function updateImage(){
-
+    
     let suffix = this.dataset.sizing || ``;
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
+let inputs = document.querySelectorAll(`input`);
 
 inputs.forEach(input => input.addEventListener(`change`, updateImage));
 inputs.forEach(input => input.addEventListener(`mouseover`, updateImage));
